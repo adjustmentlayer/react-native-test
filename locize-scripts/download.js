43bc38be0948aac227c7e6b750608848779f1c15
Vladimir Rudomanenko <vladimir.rudomanenko@rock-west.com>
2022-11-03 12:44:46 +0200

    add locize scripts

---
+++ locize-scripts/download.js
@@ -0,0 +1,12 @@
+const { execSync } = require('child_process');
+const version = process.env.REACT_APP_LOCIZE_DOWNLOAD_VERSION;
+
+const path = 'src/locales';
+
+const command = `locize download --project-id ${process.env.REACT_APP_LOCIZE_PROJECT_ID} --api-key ${process.env.REACT_APP_LOCIZE_API_KEY} --ver ${version} --path ${path} --format flat`;
+
+const runCommand = (command) => {
+    execSync(command, { stdio: [0, 1, 2] });
+};
+
+runCommand(command);
