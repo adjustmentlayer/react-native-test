4767d1e7ae3bb60f632922312d30efc563e15c85
Vladimir Rudomanenko <vladimir.rudomanenko@rock-west.com>
2022-11-03 02:42:51 +0200

    install gesture handler

--- App.js
+++ App.js
@@ -25,30 +25,34 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+      <GestureHandlerRootView>
+        <View style={styles.sectionContainer}>
+          <Text
+              style={[
+                styles.sectionTitle,
+                {
+                  color: isDarkMode ? Colors.white : Colors.black,
+                },
+              ]}>
+            {title}
+          </Text>
+          <Text
+              style={[
+                styles.sectionDescription,
+                {
+                  color: isDarkMode ? Colors.light : Colors.dark,
+                },
+              ]}>
+            {children}
+          </Text>
+        </View>
+      </GestureHandlerRootView>
+
   );
 };
 
